75a4abbee05ae8e931f2523aee4469f436f1ae1a
Night31 <32330322+Night31@users.noreply.github.com>
2018-05-17 00:01:03 +0800

    memeing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-get gud
+HAI 1.2
+   I HAS A VAR ITZ A YARN BTW DECLARE A VARIABLE FOR LATER USE
+   VISIBLE "How much is your brain"
+   GIMMEH VAR BTW GET INPUT (STRING) INTO VARIABLE
+   IF VAR =>50
+      O RLY?
+         YA RLY
+            VISIBLE "Gitgud"
+      NO WAI
+            VISIBLE "You are passed, your IQ is too big for this problem"
+      OIC
+KTHXBYE
